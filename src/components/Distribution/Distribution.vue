e5f4623bb07395d53d49d2cac99c714cf9561d13
DAIXIAOYI\Daixiaoyi <510250928@qq.ocm>
2018-06-29 11:01:05 +0800

    配电拓扑文件结构初始化

---
+++ src/components/Distribution/Distribution.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid>
+    <div>{{text}}</div>
+    <distribution-topology></distribution-topology>
+  </v-container>
+</template>
+<script>
+  import DistributionTopology from '@/components/distribution/DistributionTopology'
+  export default {
+    data() {
+      return {
+        text:'hello'
+      }
+    },
+    components:{
+      "distribution-topology": DistributionTopology
+    }
+  }
+</script>
---
+++ src/components/Distribution/DistributionTopology.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid>
+    <div>{{text}}</div>
+  </v-container>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        text:'hello'
+      }
+    }
+  }
+</script>
