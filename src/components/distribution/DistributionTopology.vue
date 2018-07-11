07d45f73faf2e99bba6feb9b0b84450c076a9598
DAIXIAOYI\Daixiaoyi <510250928@qq.ocm>
2018-07-11 14:09:10 +0800

    优化js结构，封装方法，销毁conponent时销毁所有connector，新增滚动条等等

--- src/components/distribution/DistributionTopology.vue
+++ src/components/distribution/DistributionTopology.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-container fluid>
-    <div id="drawing">
+  <v-container fluid >
+    <div id="drawing" style=" overflow-y:auto; overflow-x:auto; width:1000px; height:650px;">
       <div id="start_point_1"></div>
       <div id="start_point_2"></div>
       <div id="middle_point_1"></div>
@@ -213,7 +213,7 @@
       <div id="table_6">
         <table border="1" cellspacing="0">
           <tr>
-            <td>ppPUE</td>
+            <td>pPUE</td>
             <td>PIT</td>
             <td>PC</td>
             <td>PP</td>
@@ -413,180 +413,109 @@
   export default {
     data() {
       return {
-        rItems: ['01', '02', '03', '04', '05', '06', '07', '08']
+        _jsPlumb : null,
+        firstInstance : null,
       }
     },
-    mounted() {
-      var _jsPlumb = jsPlumb.jsPlumb;
-      var firstInstance = _jsPlumb.getInstance({
-        ConnectionsDetachable: false,
-        Connector: ["Straight"],
-        Endpoint: ["Dot", {radius: 1}],
-        PaintStyle: {strokeWidth: 2, stroke: "#779966"}
-      });
-
-      var _addEndpoints = function (toId, sourceAnchors, targetAnchors) {
+    methods: {
+      jsPlumbInit(){
+        this._jsPlumb = jsPlumb.jsPlumb;
+        this.firstInstance = this._jsPlumb.getInstance({
+          ConnectionsDetachable: false,
+          Connector: ["Straight"],
+          Endpoint: ["Dot", {radius: 1}],
+          PaintStyle: {strokeWidth: 2, stroke: "#779966"}
+        });
+      },
+      addEndpoints(toId, sourceAnchors, targetAnchors){
         for (var i = 0; i < sourceAnchors.length; i++) {
           var sourceUUID = toId + sourceAnchors[i];
-          firstInstance.addEndpoint(toId, {
+          this.firstInstance.addEndpoint(toId, {
             anchor: sourceAnchors[i], uuid: sourceUUID, maxConnections: 10
           });
         }
         for (var j = 0; j < targetAnchors.length; j++) {
           var targetUUID = toId + targetAnchors[j];
-          firstInstance.addEndpoint(toId, {
+          this.firstInstance.addEndpoint(toId, {
             anchor: targetAnchors[j], uuid: targetUUID, maxConnections: 10
           });
         }
-      };
+      },
+      endPointsInit(){
+        this.addEndpoints("start_point_1", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("start_point_2", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("middle_point_1", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("yuedengyu1", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("yuedengyu2", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("yuedengyu3", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("yuedengyu4", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("dianchizu", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("jiankong", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("ATS", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("TP1", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("TP2", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QA1", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QA2", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QA3", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QB1", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QC50", ["TopCenter", "BottomCenter"], ["TopCenter", "BottomCenter"]);
+        this.addEndpoints("QF01", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QF11", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QF12", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QPR1", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QPR2", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("QPR3", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("table_4", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
+        this.addEndpoints("table_5", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
+        for (var i = 1; i < 9; i++) {
+          this.addEndpoints("QF"+i, ["TopCenter", "BottomCenter"], ["TopCenter", "BottomCenter"]);
+          this.addEndpoints("QC"+i, ["TopCenter", "BottomCenter"], ["TopCenter", "BottomCenter"]);
+          this.addEndpoints("R0"+i, ["TopCenter", "BottomCenter"], ["TopCenter", "BottomCenter"]);
+        }
+      },
+      connectionsInit(){
+        this.firstInstance.connect({uuids: ["start_point_1RightMiddle", "QA1LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QA1RightMiddle", "TP1LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["TP1RightMiddle", "ATSLeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["QA3RightMiddle", "ATSLeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["ATSRightMiddle", "QPR1LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["ATSRightMiddle", "QPR2LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["ATSRightMiddle", "QPR3LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["QPR1RightMiddle", "yuedengyu1LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QPR2RightMiddle", "yuedengyu2LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QPR3RightMiddle", "yuedengyu3LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["yuedengyu1RightMiddle", "middle_point_1LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["yuedengyu2RightMiddle", "middle_point_1LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["yuedengyu3RightMiddle", "middle_point_1LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["TP2BottomCenter", "QF11LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["TP2BottomCenter", "QF12LeftMiddle"], connector: ["Flowchart"]})
+        this.firstInstance.connect({uuids: ["middle_point_1BottomCenter", "QC50TopCenter"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QA3LeftMiddle", "TP2TopCenter"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["QF11RightMiddle", "table_4LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QF12RightMiddle", "table_5LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["start_point_2RightMiddle", "QA2LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QA2RightMiddle", "TP2LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["middle_point_1RightMiddle", "QB1LeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QB1RightMiddle", "dianchizuLeftMiddle"], connector: ["Straight"]})
+        this.firstInstance.connect({uuids: ["QB1RightMiddle", "QF01LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["QF01RightMiddle", "yuedengyu4LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        this.firstInstance.connect({uuids: ["yuedengyu4RightMiddle", "jiankongLeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
+        for (var i = 1; i < 9; i++) {
+          this.firstInstance.connect({uuids: ["QC50BottomCenter", "QC" + i+"TopCenter"], connector: ["Flowchart"]})
+          this.firstInstance.connect({uuids: ["QC"+i+"BottomCenter", "R0" + i+"TopCenter"], connector: ["Flowchart"]})
+          this.firstInstance.connect({uuids: ["R0"+i+"BottomCenter", "QF" + i+"TopCenter"], connector: ["Straight"]})
+          this.firstInstance.connect({uuids: ["TP2RightMiddle", "QF"+i+"BottomCenter"], connector: ["Flowchart", {stub: [0, 0]}]})
+        }
+      }
 
-      _addEndpoints("TP2", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
-      _addEndpoints("QF11", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
-      _addEndpoints("QF12", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
-      _addEndpoints("QC50", ["TopCenter", "BottomCenter"], ["TopCenter", "BottomCenter"]);
-      _addEndpoints("middle_point_1", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
-      _addEndpoints("QA2", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
-      _addEndpoints("QA3", ["LeftMiddle", "RightMiddle"], ["LeftMiddle", "RightMiddle"]);
-      _addEndpoints("table_4", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
-      _addEndpoints("table_5", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
-      _addEndpoints("start_point_2", ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter", "LeftMiddle", "RightMiddle"]);
-      for (var i = 1; i < 9; i++) {
-        var id = "QF" + i;
-        _addEndpoints(id, [], ["BottomCenter"]);
-        firstInstance.connect({
-          uuids: ["TP2RightMiddle", id + "BottomCenter"],
-          connector: ["Flowchart", {stub: [0, 0]}]
-        })
-      }
-      firstInstance.connect({uuids: ["TP2BottomCenter", "QF11LeftMiddle"], connector: ["Flowchart", {stub: [0, 0]}]})
-      firstInstance.connect({uuids: ["TP2BottomCenter", "QF12LeftMiddle"], connector: ["Flowchart"]})
-      firstInstance.connect({uuids: ["middle_point_1BottomCenter", "QC50TopCenter"], connector: ["Straight"]})
-      firstInstance.connect({uuids: ["QA3LeftMiddle", "TP2TopCenter"], connector: ["Flowchart", {stub: [0, 0]}]})
-      firstInstance.connect({uuids: ["QF11RightMiddle", "table_4LeftMiddle"], connector: ["Straight"]})
-      firstInstance.connect({uuids: ["QF12RightMiddle", "table_5LeftMiddle"], connector: ["Straight"]})
-      firstInstance.connect({uuids: ["start_point_2RightMiddle", "QA2LeftMiddle"], connector: ["Straight"]})
-      firstInstance.connect({uuids: ["QA2RightMiddle", "TP2LeftMiddle"], connector: ["Straight"]})
-
-      for (var i = 1; i < 9; i++) {
-        firstInstance.addEndpoint("QC" + i, {
-          anchor: "TopCenter", uuid: "EndPoint_QC" + i
-        });
-      }
-
-      firstInstance.connect({
-        source: 'start_point_1',
-        target: 'QA1',
-        anchors: [[0, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'QA1',
-        target: 'TP1',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0.2, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'TP1',
-        target: 'ATS',
-        anchors: [[0.8, 0.5, 0, 0, 0, 0], [0, 0.2, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'QA3',
-        target: 'ATS',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.8, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'ATS',
-        target: 'QPR1',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      firstInstance.connect({
-        source: 'ATS',
-        target: 'QPR2',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'ATS',
-        target: 'QPR3',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      firstInstance.connect({
-        source: 'QPR1',
-        target: 'yuedengyu1',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'QPR2',
-        target: 'yuedengyu2',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'QPR3',
-        target: 'yuedengyu3',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'middle_point_1',
-        target: 'QB1',
-        anchors: [[0.5, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      firstInstance.connect({
-        source: 'yuedengyu1',
-        target: 'middle_point_1',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0.5, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      for (var i = 1; i < 9; i++) {
-        firstInstance.connect({uuids: ["QC50BottomCenter", "EndPoint_QC" + i], connector: ["Flowchart"]})
-      }
-      for (var i = 1; i < 9; i++) {
-        firstInstance.connect({
-          source: 'QC' + i,
-          target: 'R0' + i,
-          anchors: [[0.5, 1, 0, 0, 0, 0], [0.5, 0, 0, 0, 0, 0]],
-        });
-      }
-      for (var i = 1; i < 9; i++) {
-        firstInstance.connect({
-          source: 'R0' + i,
-          target: 'QF' + i,
-          anchors: [[0.5, 0.91, 0, 0, 0, 0], [0.5, 0, 0, 0, 0, 0]],
-        });
-      }
-      firstInstance.connect({
-        source: 'yuedengyu2',
-        target: 'middle_point_1',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0.5, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      firstInstance.connect({
-        source: 'yuedengyu3',
-        target: 'middle_point_1',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0.5, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      firstInstance.connect({
-        source: 'QB1',
-        target: 'dianchizu',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'QB1',
-        target: 'QF01',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-        connector: ["Flowchart"],
-      });
-      firstInstance.connect({
-        source: 'QF01',
-        target: 'yuedengyu4',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
-      firstInstance.connect({
-        source: 'yuedengyu4',
-        target: 'jiankong',
-        anchors: [[1, 0.5, 0, 0, 0, 0], [0, 0.5, 0, 0, 0, 0]],
-      });
+    },
+    mounted() {
+      this.jsPlumbInit();
+      this.endPointsInit();
+      this.connectionsInit();
+    },
+    beforeDestroy () {
+      this.firstInstance.deleteEveryConnection()
     }
   }
 </script>
@@ -744,64 +673,56 @@
     position: absolute;
     left: 200px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R02 {
     position: absolute;
     left: 280px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R03 {
     position: absolute;
     left: 360px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R04 {
     position: absolute;
     left: 440px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R05 {
     position: absolute;
     left: 520px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R06 {
     position: absolute;
     left: 600px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R07 {
     position: absolute;
     left: 680px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #R08 {
     position: absolute;
     left: 760px;
     top: 390px;
-    width: 60px;
-    height: 200px !important;
+    width: 60px !important;
   }
 
   #QC50 {
